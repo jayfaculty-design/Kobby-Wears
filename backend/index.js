2e0340f826e710bc1b3b55273a8f5412275c0dea
Jay Designs <izfaculty12@gmail.com>
2025-03-07 18:47:52 +0000

    done

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,17 @@ const pool = new Pool({
   // database: "kobby-wears",
   // password: "facultyf",
   // port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false,
 
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: process.env.DATABASE_URL,
 });
 
 app.post("/register", async (req, res) => {
