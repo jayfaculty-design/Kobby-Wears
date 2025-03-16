ec013e8879ad43de491aa6ec174163a4430d46fb
Jay Designs <izfaculty12@gmail.com>
2025-03-16 21:30:44 +0000

    dfd

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ if (!secretKey) {
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "kobby-wears.vercel.app",
+    origin: process.env.FRONTEND_URL || "https://kobby-wears.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
